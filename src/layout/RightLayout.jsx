0312220877aeab53e2ba9676c40c2555ca68c0fd
Achmad96 <totsukamail3278@gmail.com>
2023-10-06 14:41:42 +0700

    modified RightLayout.jsx

--- src/layout/RightLayout.jsx
+++ src/layout/RightLayout.jsx
@@ -40,11 +40,11 @@ export default function RightLayout(props){
     }, [token, props.playlist]);
 
     const handleScroll = (e) => {
-        if ((playlistEl.current?.getBoundingClientRect().bottom + 180) < e.target.scrollTop) {
+        if ((playlistEl.current?.getBoundingClientRect().bottom + 180) <= e.target.scrollTop) {
             dispatch({type:  "playlist"});
         }
         
-        if ((description.current?.getBoundingClientRect().top + 180) <= e.target.scrollTop) {
+        if ((description.current?.getBoundingClientRect().top + 182) <= e.target.scrollTop) {
             dispatch({type:  "description"});
         }
         
@@ -52,14 +52,14 @@ export default function RightLayout(props){
             dispatch({type:  "p_none"});
         }
         
-        if ((description.current?.getBoundingClientRect().top + 180) > e.target.scrollTop) {
+        if ((description.current?.getBoundingClientRect().top + 182) > e.target.scrollTop) {
             dispatch({type:  "d_none"});
         }
 
     }
 
     return (
-        <div className="flex flex-col w-full pt-3 h-[85vh] bg-gradient-to-b from-[#39E08C] via-[#228554] to-[black] via-50% to-90% overflow-y-auto text-white ml-2 mt-3 rounded-t-xl" onScroll={handleScroll}>
+        <div className="flex flex-col w-full pt-3 h-[85vh] bg-gradient-to-b from-[#39E08C] to-[black] to-90% overflow-y-auto text-white ml-2 mt-3 rounded-t-xl" onScroll={handleScroll}>
             <div ref={playlistEl} className="text-7xl mb-20 font-bold">
                 {props.playlist?.name && (
                     <div className="flex flex-row items-end transition ease-in-out duration-300 py-5 gap-3 pl-10">
@@ -74,7 +74,7 @@ export default function RightLayout(props){
                     </div>
                 )}
             </div>
-            <div className="flex flex-col relative before:absolute before:bg-black before:opacity-75 before:top-0 before:left-0 before:right-0 before:bottom-0">
+            <div className="flex flex-col relative before:absolute before:bg-black before:opacity-50 before:top-0 before:left-0 before:right-0 before:bottom-0">
                 <div ref={description} className="flex border-b mb-3 w-full items-center transition ease-in-out duration-300 opacity-70">
                     <div className="p-3 ml-6 ">#</div>
                     <div className="ml-9">Judul</div>
@@ -83,18 +83,18 @@ export default function RightLayout(props){
                 <AnimatePresence>
                     {
                         state.playlist && (
-                                <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity:0}} transition={{ ease: "easeInOut", duration: .3 }} className="flex flex-row items-end fixed top-3 bg-[#228554] w-[83%] rounded-tl-xl z-10 py-3 gap-3 pl-10">
+                                <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity:0}} transition={{ ease: "easeInOut", duration: .3 }} className="flex flex-row items-end fixed top-3 bg-[#228554] w-[83%] rounded-tl-xl z-50 py-3 gap-3 pl-10">
                                     <img src={props.playlist?.img} alt="song" className="w-10 h-10 shadow-2xl"></img>
                                     <div className="flex flex-col pl-3">
                                         <p className="text-xl">{props.playlist?.name}</p>
-                                        <p className="text-sm">{props.playlist?.owner}</p>
+                                        <p className="text-sm ">{props.playlist?.owner}</p>
                                     </div>
                                 </motion.div>
                         )
                     }
                     {
                         state.description && (
-                                <motion.div initial={{opacity: 0}} animate={{opacity: .9}} exit={{opacity:0}} transition={{ ease: "easeInOut", duration: .3 }} className="flex fixed z-10 bg-[#165D3A] w-[83%] top-[83px] mb-3 items-center">
+                                <motion.div initial={{opacity: 0}} animate={{opacity: .9}} exit={{opacity:0}} transition={{ ease: "easeInOut", duration: .1 }} className="flex fixed z-10 bg-[#165D3A] w-[83%] top-[83px] mb-3 items-center">
                                     <div className="p-3 ml-6 ">#</div>
                                     <div className="ml-9">Judul</div>
                                     <IoTimeSharp className="ml-[70%]"/>
@@ -106,7 +106,7 @@ export default function RightLayout(props){
                     const name = v.track.name;
                     const img = v.track.album.images[0].url;
                     const artists = v.track.artists.map(v => v.name).join(", ");
-                    const class_name = `flex ${i < 9 ? 'ml-[18px]' : 'ml-[10px]'} p-3 gap-3 w-full hover:bg-emerald-700`;
+                    const class_name = `flex ${i < 9 ? 'ml-[18px]' : 'ml-[10px]'} p-3 gap-3 w-full hover:bg-[#2B3731]`;
                         return (
                             <div key={v.track.id} className="flex relative items-center ml-5">
                                 <p className="p-5">{i+1}</p>
