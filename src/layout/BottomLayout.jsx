034e1ffcff361bb04cb94c987c299c393a683642
Achmad96 <totsukamail3278@gmail.com>
2023-10-15 16:04:37 +0700

    modified BottomLayout.jsx

--- src/layout/BottomLayout.jsx
+++ src/layout/BottomLayout.jsx
@@ -16,35 +16,43 @@ export default function BottomLayout() {
 
     const handleSong = async () => {
         const device_id = "16bd04e11348489421f2cd765d3b07b70fa8ecfb";
+        const context_uri = "spotify:playlist:6AGdPxT2tGSKcbSGhGaylj";
         if (isPlaying) {
-            setPlaying(false);
-            await fetchHandler(token, `/v1/me/player/pause?device_id=${device_id}`, "put");
-        } else {
-            await fetchHandler(token, `/v1/me/player/play?device_id=${device_id}`, "put", {
-                body: {
-                    offset: {
-                        position: 0,
-                    },
-                    position_ms: 0,
-                },
-            }).catch((error) => console.log(error));
-            setPlaying(true);
+            return await fetchHandler(
+                token,
+                `/v1/me/player/pause?device_id=${device_id}`,
+                "put"
+            ).then(() => setPlaying(false));
         }
+
+        return await fetchHandler(token, `/v1/me/player/play?device_id=${device_id}`, "put", {
+            body: {
+                context_uri,
+                offset: {
+                    position: 0,
+                },
+                position_ms: 0,
+            },
+        })
+            .then(() => setPlaying(true))
+            .catch((error) => console.log(error));
     };
 
     useEffect(() => {
-        const callData = async () => {
-            if (token !== undefined) {
-                const res = await fetchHandler(token, "/v1/me/player/currently-playing");
-                if (res.data.is_playing) setPlaying(true);
-                setSongObject(res.data.item);
-            }
-        };
-        callData();
-    }, [token]);
+        if (token) {
+            setTimeout(() => {
+                const callData = async () => {
+                    const response = await fetchHandler(token, "/v1/me/player/currently-playing");
+                    if (response.data.is_playing && isPlaying) setPlaying(true);
+                    setSongObject(response.data.item);
+                };
+                callData();
+            }, 5000);
+        }
+    }, [token, isPlaying, songObject]);
     return (
-        <footer className="flex items-center justify-between z-10 bg-[#121212] w-[100%] h-[15vh] text-white gap-5">
-            <div className="ml-5">
+        <footer className="flex items-center z-10 bg-[#121212] w-[100%] h-[15vh] text-white gap-5">
+            <div className="ml-5 w-[43rem]">
                 {songObject?.album && (
                     <div className="flex gap-1">
                         <img
@@ -54,26 +62,26 @@ export default function BottomLayout() {
                         ></img>
                         <div className="ml-3 mb-3 flex justify-end flex-col">
                             <p className="text-sm">{songObject?.name}</p>
-                            <p className="text-xs">
+                            <p className="text-xs opacity-60">
                                 {songObject?.artists.map((v) => v.name).join(", ")}
                             </p>
                         </div>
                     </div>
                 )}
             </div>
-            <div className="flex flex-row gap-5">
+            <div className="flex flex-row items-center gap-5">
                 <BsSkipStartFill
                     className="w-7 h-7 opacity-70 hover:opacity-100"
                     onClick={() => handlePrevAndNext("previous")}
                 />
                 {isPlaying ? (
                     <FaPauseCircle
-                        className="w-8 h-8 opacity-70 hover:opacity-100"
+                        className="w-9 h-9 opacity-70 hover:opacity-100"
                         onClick={handleSong}
                     />
                 ) : (
                     <FaPlayCircle
-                        className="w-8 h-8 opacity-70 hover:opacity-100"
+                        className="w-9 h-9 opacity-70 hover:opacity-100"
                         onClick={handleSong}
                     />
                 )}
@@ -82,7 +90,9 @@ export default function BottomLayout() {
                     onClick={() => handlePrevAndNext("next")}
                 />
             </div>
-            <input type="range" defaultValue="50" className="w-24 h-1 mr-10 " />
+            <div className="w-[43rem] flex justify-end">
+                <input type="range" defaultValue="50" className="w-24 h-1 mr-10 " />
+            </div>
         </footer>
     );
 }
